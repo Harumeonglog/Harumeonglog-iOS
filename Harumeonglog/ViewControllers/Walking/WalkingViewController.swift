4d93faa22b33483b767ec845be811259f4d59871
m1nzez <vivi020405@naver.com>
2025-03-19 16:33:34 +0900

    Feat: 현위치 트래킹 및 마커표시(#2)

--- Harumeonglog/ViewControllers/Walking/WalkingViewController.swift
+++ Harumeonglog/ViewControllers/Walking/WalkingViewController.swift
@@ -15,6 +15,9 @@ class WalkingViewController: UIViewController {
     private let minHeight: CGFloat = 150
     private let maxHeight: CGFloat = 750
     
+    private var locationManager = CLLocationManager()
+    private var userLocationMarker: NMFMarker?      // 네이버지도에서 마커 객체 선언
+
     private lazy var walkingView: WalkingView = {
         let view = WalkingView()
         
@@ -32,14 +35,11 @@ class WalkingViewController: UIViewController {
         
         walkingView.recommendRouteTableView.delegate = self
         walkingView.recommendRouteTableView.dataSource = self
-    
-    }
-    
-    // 현재 위치로 이동하는 함수
-    @objc func moveToUserLocationButtonTapped() {
         
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest    // 거리 정확도 설정
+        locationManager.requestWhenInUseAuthorization()              // 서비스 권한을 허용할 것인지 묻는 팝업    }
     }
-    
     
     @objc func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
           let translation = gesture.translation(in: walkingView.recommendRouteView)
@@ -70,13 +70,105 @@ class WalkingViewController: UIViewController {
               UIView.animate(withDuration: 0.3) {
                   self.view.layoutIfNeeded()
               }
-              
           default:
               break
           }
       }
 }
 
+extension WalkingViewController: CLLocationManagerDelegate {
+    
+    // 현재 위치로 이동하는 함수
+    @objc func moveToUserLocationButtonTapped() {
+        if CLLocationManager.locationServicesEnabled() {
+            print("위치 서비스 on 상태")
+            
+            let status = locationManager.authorizationStatus
+            
+            switch status {
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()     // 최초 요청
+            case .denied, .restricted:
+                showLocationPermissionAlert()                       // 설정으로 이동하도록 유도
+            case .authorizedWhenInUse, .authorizedAlways:
+                // 위치 업데이트가 아직 시작되지 않았다면 시작
+                if locationManager.location == nil {
+                    locationManager.startUpdatingLocation()  // 처음 위치를 받아오기 시작
+                }
+                // 위치 업데이트 없이 현재 위치로 이동하는 함수 호출
+                moveCameraToCurrentLocation()
+
+            @unknown default:
+                break
+            }
+        } else {
+            print("위치 서비스 off 상태")
+            // 위치 서비스 자체가 꺼진 경우 (기기 설정에서 GPS OFF)
+            showLocationPermissionAlert()
+        }
+    }
+    
+    // 위치가 이동할 때마다 위치 정보 업데이트
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("didUpdateLocations")
+        // 가장 최근에 받아온 위치
+        if let location = locations.first {
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            
+            print("위도: \(latitude), 경도: \(longitude)")
+        }
+    }
+
+    // 위치 정보를 기반으로 카메라 이동 & 마커 이동
+    func moveCameraToCurrentLocation() {
+        if let location = locationManager.location {
+            print("위도: \(location.coordinate.latitude), 경도: \(location.coordinate.longitude)")
+            
+            let userLatLng = NMGLatLng(lat: location.coordinate.latitude, lng: location.coordinate.longitude)
+            let cameraUpdate = NMFCameraUpdate(scrollTo: userLatLng)
+            cameraUpdate.animation = .easeIn
+            walkingView.naverMapView.mapView.moveCamera(cameraUpdate)
+            
+            let marker = NMFMarker()
+            marker.width = 35
+            marker.height = 40
+            marker.position = userLatLng
+            marker.iconImage = NMFOverlayImage(image: UIImage(named: "currentLocation")!)
+            marker.mapView = walkingView.naverMapView.mapView
+            
+        } else {
+            print("위치 정보가 없습니다.")
+            // 위치 정보가 없으면 기본 위치로 이동
+            let userLatLng = NMGLatLng(lat: 37.5665, lng: 126.9780)
+            let cameraUpdate = NMFCameraUpdate(scrollTo: userLatLng)
+            cameraUpdate.animation = .easeIn
+            walkingView.naverMapView.mapView.moveCamera(cameraUpdate)
+        }
+    }
+
+    // 위도 경도 받아오기 에러
+     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+         print(error)
+     }
+    
+    // 위치 접근 권한 허용안한 경우 설정에 들어가도록 유도
+    func showLocationPermissionAlert() {
+        let alert = UIAlertController(title: "위치 권한 필요",
+                                      message: "현재 위치를 사용하려면 설정에서\n 위치 접근을 허용해 주세요.",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "설정으로 이동", style: .default, handler: { _ in
+            if let appSettings = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSettings)
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+// 추천 경로에 대한 tableView
 extension WalkingViewController: UITableViewDelegate, UITableViewDataSource {
     // 셀 등록
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
