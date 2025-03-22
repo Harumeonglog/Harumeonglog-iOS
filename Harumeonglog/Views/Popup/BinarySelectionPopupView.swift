35a4afe7b8d577fd386a20035a09c4074e668632
Rudy-009 <rofxnaos@gmail.com>
2025-03-23 00:12:54 +0900

    feat: 팝업뷰 UI 구현

---
+++ Harumeonglog/Views/Popup/BinarySelectionPopupView.swift
@@ -0,0 +1,103 @@
+//
+//  PopupView.swift
+//  ToYou
+//
+//  Created by 이승준 on 2/1/25.
+//
+
+import UIKit
+
+class BinarySelectionPopupView: UIView {
+    
+    public lazy var mainFrame = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 15
+        $0.clipsToBounds = true
+    }
+    
+    public lazy var mainTitleLabel = UILabel().then {
+        $0.numberOfLines = 2
+        $0.textAlignment = .center
+        $0.textColor = .black
+    }
+    
+    private lazy var divider = UIView().then {
+        $0.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3)
+    }
+    
+    private lazy var buttonStackFrame = UIView()
+    
+    public lazy var leftConfirmButton = UIButton().then {
+        $0.setTitleColor(.black, for: .normal)
+    }
+    
+    public lazy var rightConfirmButton = UIButton().then {
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.4)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.black.withAlphaComponent(0.1)
+        self.addComponents()
+    }
+    
+    private func addComponents() {
+        
+        self.addSubview(mainFrame)
+        
+        mainFrame.snp.makeConstraints { make in
+            make.height.equalTo(200)
+            make.width.equalTo(300)
+            make.center.equalToSuperview()
+        }
+        
+        mainFrame.addSubview(mainTitleLabel)
+        mainFrame.addSubview(divider)
+        mainFrame.addSubview(buttonStackFrame)
+        
+        mainTitleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-30)
+        }
+        
+        divider.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(2)
+            make.height.equalTo(1)
+            make.top.equalTo(buttonStackFrame.snp.top)
+        }
+        
+        buttonStackFrame.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalTo(300)
+            make.height.equalTo(60)
+            make.bottom.equalToSuperview().inset(12)
+        }
+        
+        buttonStackFrame.addSubview(leftConfirmButton)
+        buttonStackFrame.addSubview(rightConfirmButton)
+        
+        leftConfirmButton.snp.makeConstraints { make in
+            make.leading.top.bottom.equalToSuperview()
+            make.width.equalTo(150)
+            make.height.equalTo(60)
+        }
+        
+        rightConfirmButton.snp.makeConstraints { make in
+            make.trailing.top.bottom.equalToSuperview()
+            make.width.equalTo(150)
+            make.height.equalTo(60)
+        }
+    }
+    
+    public func configure(title: String, leftButtonText: String, rightButtonText: String) {
+        mainTitleLabel.text = title
+        leftConfirmButton.setTitle(leftButtonText, for: .normal)
+        rightConfirmButton.setTitle(rightButtonText, for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
