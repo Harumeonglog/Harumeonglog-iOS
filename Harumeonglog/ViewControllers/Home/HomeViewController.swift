aa7c8dca2c6d51e7d4ac59159d3555ad90bb36d9
Dana Lim <109071777+danajlim@users.noreply.github.com>
2025-03-24 15:28:38 +0900

    Revert "Feature/home"

---
+++ Harumeonglog/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Harumeonglog
+//
+//  Created by 이승준 on 3/13/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
