d2b18e91be5c448e7da1c18698539f71dcf15ecb
Rudy-009 <rofxnaos@gmail.com>
2025-04-07 11:44:30 +0900

    feat: 초대하기 유저 스테이지 UI 구현

--- Harumeonglog/My Page/View/InviteUserView.swift
+++ Harumeonglog/My Page/View/InviteUserView.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class InviteUserView: UIView {
+class InviteUserView: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
     
     private lazy var searchTextField = UITextField().then {
         $0.layer.cornerRadius = 20
         $0.clipsToBounds = true
+        $0.font = UIFont.body
         
         $0.backgroundColor = .white
         $0.tintColor = .gray02
@@ -19,6 +20,21 @@ class InviteUserView: UIView {
         $0.layer.borderWidth = 1
         $0.layer.borderColor = UIColor.brown02.cgColor
     }
+    
+    public lazy var userStageCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 20
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .background
+        cv.showsHorizontalScrollIndicator = false
+        cv.showsVerticalScrollIndicator = false
+        cv.bouncesHorizontally = false
+        
+        cv.register(UserStageCell.self, forCellWithReuseIdentifier: UserStageCell.identifier)
+        return cv
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -35,8 +51,26 @@ class InviteUserView: UIView {
             make.height.equalTo(40)
         }
         addLeftViewInTextField()
+        
+        self.addSubview(userStageCollectionView)
+        
+        userStageCollectionView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.trailing.equalToSuperview()
+            make.top.equalTo(searchTextField.snp.bottom).offset(40)
+            make.bottom.equalToSuperview().inset(20)
+        }
     }
     
+    // MARK: CollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+
     private func addLeftViewInTextField() {
         let leftView = UIView(frame: CGRect(x: 0, y: 0, width: 55, height: 30))
         
--- Harumeonglog/My Page/ViewController/InviteUserViewController.swift
+++ Harumeonglog/My Page/ViewController/InviteUserViewController.swift
@@ -14,6 +14,31 @@ class InviteUserViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = inviteUserView
+        self.inviteUserView.userStageCollectionView.delegate = self
+        self.inviteUserView.userStageCollectionView.dataSource = self
+
+    }
+}
+
+extension InviteUserViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return UserStageModel.data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let data = UserStageModel.data[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UserStageCell.identifier, for: indexPath) as! UserStageCell
+        cell.configure(data: data)
+        return cell
+    }
+
+}
+
+extension InviteUserViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width - 40, height: 50)
     }
 }
 
