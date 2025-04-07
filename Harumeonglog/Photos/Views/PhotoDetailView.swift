325b9f659481d90b8b55bf8e9743c632f5ec2b27
danajlim <danalim0819@gmail.com>
2025-04-07 21:09:15 +0900

    add: 일단 이상한데 사진 선택 버튼 동작 추가

--- Harumeonglog/Photos/Views/PhotoDetailView.swift
+++ Harumeonglog/Photos/Views/PhotoDetailView.swift
@@ -22,6 +22,29 @@ class PhotoDetailView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    let bottomActionBar : UIView = {
+        let view = UIView()
+        view.layer.borderColor = UIColor.gray04.cgColor
+        view.layer.borderWidth = 1
+        view.backgroundColor = .bg
+        view.isHidden = true
+        return view
+    }()
+    
+    let downloadButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "arrow.down.to.line.compact"), for: .normal)
+        button.tintColor = .gray00
+        return button
+    }()
+    
+    let deleteButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "trash"), for: .normal)
+        button.tintColor = .gray00
+        return button
+    }()
 
     private func setupView() {
         imageView.contentMode = .scaleAspectFit
@@ -29,6 +52,9 @@ class PhotoDetailView: UIView {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(imageView)
         addSubview(navigationBar)
+        addSubview(bottomActionBar)
+        bottomActionBar.addSubview(downloadButton)
+        bottomActionBar.addSubview(deleteButton)
         
         imageView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -39,6 +65,23 @@ class PhotoDetailView: UIView {
         
         navigationBar.snp.makeConstraints { make in
             make.leading.top.trailing.equalTo(self.safeAreaLayoutGuide)
+        }
+        
+        bottomActionBar.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(87)
+        }
+        
+        downloadButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(33)
+            make.bottom.equalToSuperview().inset(37)
+            make.width.height.equalTo(24)
+        }
+        
+        deleteButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(33)
+            make.centerY.equalTo(downloadButton)
+            make.width.height.equalTo(24)
         }
     }
 }
