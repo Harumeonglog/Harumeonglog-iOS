08bde12106c2ae72699e5286a90f53b3658e0d7c
Rudy-009 <rofxnaos@gmail.com>
2025-04-17 17:53:25 +0900

    fix: Kakao Login Bug

--- Harumeonglog/Login/LoginViewController.swift
+++ Harumeonglog/Login/LoginViewController.swift
@@ -26,12 +26,7 @@ extension LoginViewController {
         self.loginView.appleLoginButton.addTarget(self, action: #selector(handleAppleLogin), for: .touchUpInside)
         self.loginView.kakaoLoginButton.addTarget(self, action: #selector(handleKakaologin), for: .touchUpInside)
     }
-    
-    @objc
-    private func handleAppleLogin() {
-        RootViewControllerService.toBaseViewController()
-    }
-    
+        
     @objc
     private func handleKakaologin() {
         KakaoLogin()
@@ -41,6 +36,7 @@ extension LoginViewController {
         // 카카오톡 실행 가능 여부 확인
         if (UserApi.isKakaoTalkLoginAvailable()) {
             // 카카오톡 앱으로 로그인 인증
+            print("111")
             kakaoLonginWithApp()
         } else { // 카톡이 설치가 안 되어 있을 때
             // 카카오 계정으로 로그인
@@ -51,13 +47,11 @@ extension LoginViewController {
     func kakaoLonginWithApp() {
         UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
             if let error = error {
-                print("카카오 APP 로그인 에러")
+                // print("카카오 APP 로그인 에러")
                 print(error)
             } else {
-                print("loginWithKakaoTalk() success.")
-                //do something
-                RootViewControllerService.toBaseViewController()
-                _ = oauthToken
+                guard let oauthToken = oauthToken else { return }
+                print(oauthToken)
             }
         }
     }
@@ -65,15 +59,22 @@ extension LoginViewController {
     func kakaoLoginWithAccount() {
         UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
             if let error = error {
-                print("카카오 WEB 로그인 에러")
+                // print("카카오 WEB 로그인 에러")
                 print(error)
             } else {
-                print("loginWithKakaoAccount() success.")
-                //do something
-                RootViewControllerService.toBaseViewController()
-                _ = oauthToken
+                guard let oauthToken = oauthToken else { return }
+                print(oauthToken)
             }
         }
+    }
+    
+    func kakaoLogin(oauthToken: String) {
+        
+    }
+    
+    @objc
+    private func handleAppleLogin() {
+        RootViewControllerService.toBaseViewController()
     }
     
 }
--- Harumeonglog/SceneDelegate.swift
+++ Harumeonglog/SceneDelegate.swift
@@ -16,19 +16,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = BaseViewController()
+        window?.rootViewController = LoginViewController()
         window?.makeKeyAndVisible()
         
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-            if let url = URLContexts.first?.url {
-                if (AuthApi.isKakaoTalkLoginUrl(url)) {
-                    _ = AuthController.handleOpenUrl(url: url)
-                }
+        if let url = URLContexts.first?.url {
+            if AuthApi.isKakaoTalkLoginUrl(url) {
+                _ = AuthController.handleOpenUrl(url: url)
             }
         }
-
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
