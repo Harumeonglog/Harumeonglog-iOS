4f5bc04d6482984d5299d2e5d3a457391d970034
Rudy-009 <rofxnaos@gmail.com>
2025-07-07 22:25:19 +0900

    feat: 내가 쓴 댓글 View 구현

---
+++ Harumeonglog/UserActivity/Cell/MyCommentCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyCommentCell.swift
+//  Harumeonglog
+//
+//  Created by 이승준 on 7/7/25.
+//
+
+import UIKit
+
+class MyCommentCell: UITableViewCell {
+    
+    static let identifier: String = "MyCommentCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
--- Harumeonglog/UserActivity/View/MyCommentsView.swift
+++ Harumeonglog/UserActivity/View/MyCommentsView.swift
@@ -5,4 +5,40 @@
 //  Created by 이승준 on 7/7/25.
 //
 
-import Foundation
+import UIKit
+
+class MyCommentsView: UIView {
+    
+    public let navigationBar = CustomNavigationBar()
+    
+    public let myCommentsTableView = UITableView().then {
+        $0.register(MyCommentCell.self, forCellReuseIdentifier: MyCommentCell.identifier)
+        $0.isScrollEnabled = true
+        $0.separatorStyle = .none
+        $0.backgroundColor = .background
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .background
+        
+        self.addSubview(navigationBar)
+        self.addSubview(myCommentsTableView)
+        
+        navigationBar.configureTitle(title: "내가 쓴 댓글")
+        navigationBar.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(self.safeAreaLayoutGuide.snp.top)
+        }
+        
+        myCommentsTableView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(navigationBar.snp.bottom)
+            make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
