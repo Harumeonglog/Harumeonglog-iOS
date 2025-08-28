d3beb611a03cf519b292850167c97c5d4743f11a
Rudy-009 <rofxnaos@gmail.com>
2025-08-28 16:28:11 +0900

    Liked  Post Cursor 기반 페이지네이션으로 변경

--- Harumeonglog/UserActivity/ViewModel/UserActivityViewModel.swift
+++ Harumeonglog/UserActivity/ViewModel/UserActivityViewModel.swift
@@ -12,27 +12,22 @@ final class UserActivityViewModel: ObservableObject {
     
     @Published var likedCursor: Int = 0
     @Published var likedPosts = [PostItem]()
-    private var likedHasNext: Bool = true
     
     @Published var myPostsCursor: Int = 0
     @Published var myPosts = [PostItem]()
-    private var myPostsHasNext: Bool = true
     
     @Published var myCommentCursor: Int = 0
     @Published var myComments = [MyCommentItem]()
-    private var myCommentsHasNext: Bool = true
     
     var cancellables: Set<AnyCancellable> = []
     
     func getmyPosts() {
-        guard myPostsHasNext else { print("#getmyPosts hasNext false"); return }
         UserActivityService.getMyPosts(cursor: myPostsCursor) { [weak self] result in
             switch result {
             case .success(let value):
                 print("#getmyPosts success")
                 self?.myPostsCursor = value.result?.cursor ?? 0
                 self?.myPosts.append(contentsOf: value.result?.items ?? [])
-                self?.myPostsHasNext = value.result?.hasNext ?? false
             case .failure(let error):
                 print(error)
             }
@@ -40,15 +35,14 @@ final class UserActivityViewModel: ObservableObject {
     }
     
     func getLikedPosts() {
-        guard likedHasNext else { print("#getmyPosts hasNext false"); return }
-        UserActivityService.getLikedPosts(cursor: myPostsCursor) { [weak self] result in
+        UserActivityService.getLikedPosts(cursor: likedCursor) { [weak self] result in
             switch result {
-            case .success(let value):
+            case .success(let response):
                 print("#getLikedPosts success")
-                print(self?.likedPosts ?? "no Liked Posts")
-                self?.likedCursor = value.result?.cursor ?? 0
-                self?.likedPosts.append(contentsOf: value.result?.items ?? [])
-                self?.likedHasNext = value.result?.hasNext ?? false
+                guard let result = response.result else { print("Result is nil"); return }
+                guard !result.items.isEmpty else { print("Empty"); return }
+                self?.likedCursor = response.result?.items.last?.postId ?? -1
+                self?.likedPosts.append(contentsOf: response.result?.items ?? [])
             case .failure(let error):
                 print(error)
             }
@@ -56,7 +50,6 @@ final class UserActivityViewModel: ObservableObject {
     }
     
     func getMyComments() {
-        guard myCommentsHasNext else { print("#getMyComments hasNext false"); return }
         UserActivityService.getMyComments(cursor: myCommentCursor) { [weak self] result in
             switch result {
             case .success(let value):
@@ -64,7 +57,6 @@ final class UserActivityViewModel: ObservableObject {
                 print(value.result?.items)
                 self?.myCommentCursor = value.result?.cursor ?? 0
                 self?.myComments.append(contentsOf: value.result?.items ?? [])
-                self?.myCommentsHasNext = value.result?.hasNext ?? false
             case .failure(let error):
                 print(error)
             }
